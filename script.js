1c71b50c105bfaeaf8ffc62cbfde5eceedec98ce
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-11-09 18:52:03 -0800

    add logic to create board

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('working');
+const container = document.getElementById('container');
+
+const numberOfPixels = 32;
+
+const createRow = (count) => {
+	const row = document.createElement('div');
+	row.classList.add('row');
+
+	for (let i = 0; i < count; i++) {
+		const pixel = document.createElement('div');
+		pixel.classList.add('pixel');
+		pixel.style.width = `${container.clientWidth / numberOfPixels}px`;
+		pixel.style.height = `${container.clientHeight / numberOfPixels}px`;
+
+		pixel.addEventListener('mouseenter', () => {
+			pixel.style.backgroundColor = '#ccc';
+		});
+
+		row.appendChild(pixel);
+	}
+	container.appendChild(row);
+};
+
+const createBoard = (numberOfPixels) => {
+	for (let i = 0; i < numberOfPixels; i++) {
+		createRow(numberOfPixels);
+	}
+};
+
+createBoard(numberOfPixels);
