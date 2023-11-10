afae57f054b2ecaa758ad88887bbb790a57cd1b5
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-11-09 20:07:19 -0800

    fixing height and width of pixels

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const container = document.getElementById('container');
+const sizeInput = document.getElementById('size-input');
+const boardSizeLabel = document.getElementById('board-size-label');
+const newBoardBtn = document.getElementById('new-board');
 
-const numberOfPixels = 32;
+sizeInput.value = 16;
+
+sizeInput.addEventListener('input', (e) => {
+	boardSizeLabel.textContent = `${e.target.value} x ${e.target.value}`;
+});
+
+const newBoard = () => {
+	container.textContent = '';
+	createBoard(sizeInput.value);
+};
 
 const createRow = (count) => {
 	const row = document.createElement('div');
@@ -9,10 +21,10 @@ const createRow = (count) => {
 	for (let i = 0; i < count; i++) {
 		const pixel = document.createElement('div');
 		pixel.classList.add('pixel');
-		pixel.style.width = `${container.clientWidth / numberOfPixels}px`;
-		pixel.style.height = `${container.clientHeight / numberOfPixels}px`;
+		pixel.style.width = `${Math.floor(container.clientWidth / count)}px`;
+		pixel.style.height = `${Math.floor(container.clientWidth / count)}px`;
 
-		pixel.addEventListener('mouseenter', () => {
+		pixel.addEventListener('mouseover', () => {
 			pixel.style.backgroundColor = '#ccc';
 		});
 
@@ -27,4 +39,6 @@ const createBoard = (numberOfPixels) => {
 	}
 };
 
-createBoard(numberOfPixels);
+newBoardBtn.addEventListener('click', newBoard);
+
+newBoard();
